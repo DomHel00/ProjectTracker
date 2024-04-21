92eaaa8bbc72c93307371dd5d5d32bbd9285d46b
Dominik Hel <domhel00@gmail.com>
2024-04-21 10:26:12 +0200

    Creating and setting up CoreData.

---
+++ CoreData/Project+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Project+CoreDataClass.swift
+//  ProjectTracker
+//
+//  Created by Dominik Hel on 21.04.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Project)
+public class Project: NSManagedObject {
+
+}
---
+++ CoreData/Project+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Project+CoreDataProperties.swift
+//  ProjectTracker
+//
+//  Created by Dominik Hel on 21.04.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Project {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Project> {
+        return NSFetchRequest<Project>(entityName: "Project")
+    }
+
+    @NSManaged public var icon: Data?
+    @NSManaged public var completionDate: Date?
+    @NSManaged public var creationDate: Date?
+    @NSManaged public var projectProgress: String?
+    @NSManaged public var projectPriority: String?
+    @NSManaged public var projectDescription: String?
+    @NSManaged public var projectTitle: String?
+
+}
+
+extension Project : Identifiable {
+
+}
