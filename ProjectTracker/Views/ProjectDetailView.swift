0b9291d8b968b95415f70e26172f661a56c8516b
Dominik Hel <domhel00@gmail.com>
2024-04-27 18:21:15 +0200

    Creating  ProjectDetailView and ProjectTableViewCell.

---
+++ ProjectTracker/Views/ProjectDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  ProjectDetailView.swift
+//  ProjectTracker
+//
+//  Created by Dominik Hel on 27.04.2024.
+//
+
+import Foundation
---
+++ ProjectTracker/Views/ProjectTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProjectTableViewCell.swift
+//  ProjectTracker
+//
+//  Created by Dominik Hel on 26.04.2024.
+//
+
+import UIKit
+
+class ProjectTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
