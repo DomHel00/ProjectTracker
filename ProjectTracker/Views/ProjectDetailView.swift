87a85d03bbf82d9e72eb7279ea769826667b4356
Dominik Hel <domhel00@gmail.com>
2024-04-27 18:23:21 +0200

    Adding default configuration for ProjectDetailView.

--- ProjectTracker/Views/ProjectDetailView.swift
+++ ProjectTracker/Views/ProjectDetailView.swift
@@ -5,4 +5,17 @@
 //  Created by Dominik Hel on 27.04.2024.
 //
 
-import Foundation
+import UIKit
+
+final class ProjectDetailView: UIView {
+    let project: Project
+    
+    init(project: Project) {
+        self.project = project
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
