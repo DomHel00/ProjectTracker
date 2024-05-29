5e7837ddae6ff70965a4a5797742f82326542551
Dominik Hel <domhel00@gmail.com>
2024-05-29 20:29:37 +0200

    Updating ProjectModel according to the CoreData model.

--- ProjectTracker/Models/ProjectModel.swift
+++ ProjectTracker/Models/ProjectModel.swift
@@ -8,19 +8,21 @@
 import Foundation
 
 struct ProjectModel {
-    var projectTitle: String?
+    var projectTitle: String
     var projectDescription: String?
-    var projectPriority: String?
-    var projectProgress: String?
-    var creationDate: Date?
+    var projectPriority: String
+    var projectProgress: String
+    var creationDate: Date
     var icon: Data?
+    var projectURL: URL?
     
-    init(projectTitle: String? = nil, projectDescription: String? = nil, projectPriority: String? = nil, projectProgress: String? = nil, creationDate: Date? = nil, icon: Data? = nil) {
+    init(projectTitle: String, projectDescription: String? = nil, projectPriority: String, projectProgress: String, creationDate: Date, icon: Data? = nil, projectURL: URL?) {
         self.projectTitle = projectTitle
         self.projectDescription = projectDescription
         self.projectPriority = projectPriority
         self.projectProgress = projectProgress
         self.creationDate = creationDate
         self.icon = icon
+        self.projectURL = projectURL
     }
 }
