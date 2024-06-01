c8bbf1e2353a37c0d7c4bca8eebb8479c112ecd7
Dominik Hel <domhel00@gmail.com>
2024-06-01 19:11:16 +0200

    Creating a SizeClassEnum.

---
+++ ProjectTracker/Others/SizeClassEnum.swift
@@ -0,0 +1,13 @@
+//
+//  SizeClassEnum.swift
+//  ProjectTracker
+//
+//  Created by Dominik Hel on 01.06.2024.
+//
+
+import Foundation
+
+enum SizeClassEnum {
+    case regular
+    case compact
+}
