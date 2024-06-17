7800fdc10b0f5887403f4e9c837d9747e0178b17
Dominik Hel <domhel00@gmail.com>
2024-06-17 17:35:31 +0200

    Creating a UserDefaultsManager for saving and loading selected sort type and selected order type to UserDefaults.

--- ProjectTracker/Managers/UserDefaultsManager.swift
+++ ProjectTracker/Managers/UserDefaultsManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+final class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    
+    private let standart = UserDefaults.standard
+    
+    private init() {}
+    
+    enum storedVariables {
+        case sortType
+        case orderType
+        
+        var forKey: String {
+            switch self {
+            case .sortType:
+                return "sortType"
+            case .orderType:
+                return "orderType"
+            }
+        }
+    }
+    
+    public func loadValue(for storedVariable: storedVariables) -> Int {
+        return standart.integer(forKey: storedVariable.forKey)
+    }
+    
+    public func setValue(for storedVariable: storedVariables, value: Int) {
+        standart.setValue(value, forKey: storedVariable.forKey)
+    }
+}
