ee3847d8004728d65f54efaa6eea998a8218a8f1
Dominik Hel <domhel00@gmail.com>
2024-07-10 12:41:16 +0200

    Adding a comments and documentation for files in Other folder.

--- ProjectTracker/Others/Extensions.swift
+++ ProjectTracker/Others/Extensions.swift
@@ -5,17 +5,19 @@
 //  Created by Dominik Hel on 01.06.2024.
 //
 
+// MARK: Imports
 import Foundation
 
+// MARK: String extension
 extension String {
+    /// Returns bool value for isValid property.
     var isValidURL: Bool {
         let regex = "^(http|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/+;\\*]*)?$"
         let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
         return predicate.evaluate(with: self)
     }
-}
-
-extension String {
+    
+    /// Returns localized string if it is possible.
     func localized() -> String {
         return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
     }
--- ProjectTracker/Others/SizeClassEnum.swift
+++ ProjectTracker/Others/SizeClassEnum.swift
@@ -5,9 +5,12 @@
 //  Created by Dominik Hel on 01.06.2024.
 //
 
+// MARK: Imports
 import Foundation
 
+// MARK: SizeClassEnum enum
 enum SizeClassEnum {
+    // MARK: Cases
     case regular
     case compact
 }
