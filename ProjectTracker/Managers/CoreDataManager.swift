44603c518c07af7966698b24916b9ac5a9f889aa
Dominik Hel <domhel00@gmail.com>
2024-07-10 13:03:03 +0200

    Adding a comments and documentation for files in Managers folder.

--- ProjectTracker/Managers/CoreDataManager.swift
+++ ProjectTracker/Managers/CoreDataManager.swift
@@ -5,17 +5,26 @@
 //  Created by Dominik Hel on 21.04.2024.
 //
 
+// MARK: Imports
 import UIKit
 import CoreData
 
+// MARK: CoreDataManager class
 final class CoreDataManager {
+    // MARK: Constants and variables
     static let shared = CoreDataManager()
     
     private let appDelegate = UIApplication.shared.delegate as! AppDelegate
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
+    // MARK: Inits
     private init() {}
     
+    // MARK: Functions
+    /// Adds a new object to the CoreData.
+    ///
+    /// - Parameters:
+    ///     - object: The object which  will be added.
     public func addObject(_ object: ProjectModel) {
         let project = Project(context: context)
         project.projectID = UUID()
@@ -29,12 +38,21 @@ final class CoreDataManager {
         appDelegate.saveContext()
     }
     
+    /// Delete a selected object from the CoreData.
+    ///
+    /// - Parameters:
+    ///     - object: The object which  will be deleted.
     public func deleteObject(_ object: Project) {
         context.delete(object)
         appDelegate.saveContext()
     }
     
-    public func updateObject(oldObject: Project, newObject: ProjectModel) {
+    /// Replaces the old object with a new object in the CoreData.
+    ///
+    /// - Parameters:
+    ///     - oldObject: The object which will be replaced.
+    ///     - newObject:  The object which will replaces it.
+    public func replaceObject(oldObject: Project, newObject: ProjectModel) {
         let fetchRequest: NSFetchRequest<Project> = Project.fetchRequest()
 
         let predicate = NSPredicate(format: "projectID == '\(oldObject.projectID)'")
--- ProjectTracker/Managers/UserDefaultsManager.swift
+++ ProjectTracker/Managers/UserDefaultsManager.swift
@@ -5,19 +5,27 @@
 //  Created by Dominik Hel on 07.06.2024.
 //
 
+// MARK: Imports
 import Foundation
 
+// MARK: UserDefaultsManager class
 final class UserDefaultsManager {
+    // MARK: Constants and variables
     static let shared = UserDefaultsManager()
     
     private let standart = UserDefaults.standard
     
+    // MARK: Inits
     private init() {}
     
-    enum storedVariables {
+    // MARK: StoredVariables enum
+    enum StoredVariables {
+        // MARK: Cases
         case sortType
         case orderType
         
+        // MARK: Computed properties
+        /// Returns forKey string for  case.
         var forKey: String {
             switch self {
             case .sortType:
@@ -28,11 +36,21 @@ final class UserDefaultsManager {
         }
     }
     
-    public func loadValue(for storedVariable: storedVariables) -> Int {
+    // MARK: Functions
+    /// Returns saved int value for selected stored variable  or default value.
+    ///
+    /// - Parameters:
+    ///     - storedVariable: The variable for which we want to retrieve the stored value.
+    public func loadValue(for storedVariable: StoredVariables) -> Int {
         return standart.integer(forKey: storedVariable.forKey)
     }
     
-    public func setValue(for storedVariable: storedVariables, value: Int) {
+    /// Sets value  for selected stored variable.
+    ///
+    /// - Parameters:
+    ///     - storedVariable: The variable for which we want to save the new value.
+    ///     - value: The value  which will be saved into the stored variable.
+    public func setValue(for storedVariable: StoredVariables, value: Int) {
         standart.setValue(value, forKey: storedVariable.forKey)
     }
 }
